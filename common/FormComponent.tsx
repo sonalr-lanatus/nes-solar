0bee0f1cea8026527b641d7bf71bedc87c00486f
sonalr-lanatus <sonal.r@lanatussystems.com>
2023-12-06 10:01:58 +0530

    update the packages

--- common/FormComponent.tsx
+++ common/FormComponent.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import Box from "@mui/material/Box/Box";
 
 export default function FormComponent() {
-  return <div></div>;
+  return (
+    <div>
+      <Box
+        sx={{
+          backgroundColor: "white",
+          minWidth: "500px",
+          marginTop: "20px",
+          border: "1px solid #203F69",
+          color: "#203F69",
+          boxShadow: "0 20px 40px 0 rgba(0,0,0,.1)",
+          height: "200px",
+          margin: "auto",
+        }}
+      >
+        Hello
+      </Box>
+    </div>
+  );
 }
